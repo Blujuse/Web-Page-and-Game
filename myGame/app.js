27443c107c9498930a377e78234b0c53825f7787
Blujuse-Will <wlhps@outlook.com>
2024-12-21 18:32:52 +0000

    Setup Login & Registration

--- myGame/app.js
+++ myGame/app.js
@@ -27,20 +27,25 @@ app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//register function
-function registerUser(Username, Password, res) {
+//
+// Register Function
+//
+function registerUser(Username, Password, res) 
+{
   const saltRounds = 10;
   const score = 0;
   console.log("1")
-  bcrypt.hash(Password, saltRounds, (err, hashedPassword)=>{
-    if (err){
+  bcrypt.hash(Password, saltRounds, (err, hashedPassword) => {
+    if (err)
+    {
       //if this is called, this means that there was an error with hashing the password
       console.error("Error hashing the password: ", err);
       return res.status(404).send('1');
     };
     const query = 'INSERT INTO user (username, password, score) VALUES (?, ?, ?)';
-    con.query(query, [Username, hashedPassword, score], (err, results)=>{
-      if (err) {
+    con.query(query, [Username, hashedPassword, score], (err, results) => {
+      if (err) 
+      {
         //this is called when there is an error inputing the user
         console.error("Error inserting user: ", err);
         return res.status(404).send('2');
@@ -53,19 +58,71 @@ function registerUser(Username, Password, res) {
 
 app.post('/register', (req, res) => {
   console.log("log")
-  const { username, passW } = req.body;
+  const { username, userPassword } = req.body;
   console.log("0")
-  registerUser(username, passW, res);
+  registerUser(username, userPassword, res);
   console.log("123")
 });
 
+//
+//Login Function
+//
+function loginUser(Username, Password, res)
+{
+  console.log('Attempting to login with username: ', Username);  // Added log for username
+  const query = 'SELECT * FROM user WHERE username = ?';  // Check if your column is named 'username'
+  
+  con.query(query, [Username], (err, results) => {
+    if (err) 
+    {
+      console.error('Error fetching user:', err);
+      return res.status(404).send('Login failed due to fetching error.');
+    }
+    
+    if (results.length > 0)
+    {
+      const user = results[0];
+      console.log('User found in database:', user);  // Added log to see user data
+
+      bcrypt.compare(Password, user.password, (err, isMatch) => {
+        if (err)
+        {
+          console.error('Error comparing passwords:', err);
+          return res.status(404).send('Login failed due to password comparison error.');
+        }
+
+        if (isMatch)
+        {
+          console.log('Login successful:', user);
+          return res.redirect('/loginPage');
+        }
+
+        console.log('Password mismatch');  // Added log if passwords don't match
+        res.status(404).send('Invalid username or password.');
+      });
+    }
+    else
+    {
+      console.log('User not found in database');  // Added log if user not found
+      res.status(404).send('Invalid username or password.');
+    }
+  });
+}
+
 app.post('/login', (req, res) => {
-  const {Username, Password} = req.body;
-  //add login function here
+  const {username, userPassword} = req.body;
+  loginUser(username, userPassword, res);
 });
 
+//
+// Page Linking Stuff
+//
 app.get('/registerPage', function(req,res){
   res.sendFile(__dirname + '/static/register.html');
+});
+
+app.get('/loginPage', function(req,res){
+  res.sendFile(__dirname + '/static/login.html');
 });
 
 app.get('*', function(req,res){
